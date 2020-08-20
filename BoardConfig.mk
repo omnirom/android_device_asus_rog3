97a56d51d87db998944a03940727f1f8eedbfc36
micky387 <mickaelsaibi@free.fr>
2020-08-20 15:06:57 +0200

    rog3: Introduce in-screen fingerprint HAL
    Change-Id: Ieba179aa369c10200c9c3f9ab277a2ab22cbb8e6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,9 @@ DEVICE_PATH := device/asus/rog3
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
+# Hidl
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/rog3_defconfig
 
--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration_ZS661KS.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes_ZS661KS.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_volumes_ZS661KS.xml
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    omni.biometrics.fingerprint.inscreen@1.0-service.asus_kona
+
 # Frameworks
 PRODUCT_PACKAGES += \
     FrameworksResOverlay
