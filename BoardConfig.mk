aed147db75ddb099cc1750e1163f4f116c10a387
micky387 <mickaelsaibi@free.fr>
2022-10-06 17:32:58 +0200

    rog3: Fix Airtrigger, iris and other Selinx denials
    Change-Id: I9430bfd6f1583d874d8257b762b2b3f8814ac79f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,8 @@ VENDOR_SECURITY_PATCH := 2022-06-05
 
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # inherit from the proprietary version
 -include vendor/asus/rog3/BoardConfigVendor.mk
