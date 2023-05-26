126aeaac8304a4f04003e68db8a1287732654437
micky387 <mickaelsaibi@free.fr>
2023-05-26 22:00:02 +0200

    rog3: Add Asus AudioWiardView and Dirac service
    remove Google AD_ID permission for diracaudio bootreceiver since theirs no ADS.
    
    W AdvertisingIdSettings: Package org.omnirom.diracaudio failed Ad Id permission check. Apps that target Android SDK 33 or higher should declare com.google.android.gms.permission.AD_ID in the app manifest to access Ad Id.
    
    Change-Id: I22cd02fa271966fbd5d5c6f83b2a5c100f967ce6

--- device.mk
+++ device.mk
@@ -32,7 +32,9 @@ PRODUCT_PACKAGES += \
 # Api
 PRODUCT_SHIPPING_API_LEVEL := 29
 
-# audio
+# Audio
+PRODUCT_PACKAGES += AsusDiracAudio
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration_ZS661KS.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_ZS661KS.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
